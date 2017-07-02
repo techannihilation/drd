be33e04664d53d7d3f318da5ba590a7c92b9324c
nixtux <davehomer@googlemail.com>
2017-07-02 11:04:14 +0100

    VERSION{v2.49.5}

--- modinfo.lua
+++ modinfo.lua
@@ -1,12 +1,12 @@
   description='The New Generation Of War',
   shortname='TA',
-  version='$VERSION', 
+  version='$VERSION',
   mutator='',
   game='',
   shortGame='TA',
   modtype=1,
   depend = {
-    'Tech Annihilation v2.49.0', 
+    'Tech Annihilation v2.49.0',
   },
   NTAI = {
     tdfpath='TA';
