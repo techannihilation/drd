90b2f57f2e3d1d2adfa14372fde83c83f8c2d996
René Jochum <rene@jochums.at>
2018-01-03 07:07:34 +0100

    Format modinfo.lua
    Signed-off-by: René Jochum <rene@jochums.at>

--- modinfo.lua
+++ modinfo.lua
@@ -1,16 +1,16 @@
-return { 
-  name='Dynamic Robot Defense',
-  description='The New Dynamic Generation Of Robot Defense',
-  shortname='TA',
-  version='$VERSION',
-  mutator='',
-  game='',
-  shortGame='TA',
-  modtype=1,
-  depend = {
-    'Tech Annihilation v3.05.0',
-  },
-  NTAI = {
-    tdfpath='TA';
-  },
+return {
+    name = "Dynamic Robot Defense",
+    description = "The New Dynamic Generation Of Robot Defense",
+    shortname = "TA",
+    version = "$VERSION",
+    mutator = "",
+    game = "",
+    shortGame = "TA",
+    modtype = 1,
+    depend = {
+        "Tech Annihilation v3.05.0"
+    },
+    NTAI = {
+        tdfpath = "TA"
+    }
 }
